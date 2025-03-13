5b4807b8dd0df92ee03fcab451dbb93641ecc6d6
tyler36 <tyler36@users.noreply.github.com>
2025-03-13 12:59:49 +0900

    feat: always declare colors in oklch() format (#7)
    Co-authored-by: tyler36 <7234392+tyler36@users.noreply.github.com>

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -2,6 +2,9 @@
 export default {
   extends: ['stylelint-config-standard'],
   rules: {
-        'block-no-empty': true
-    }
+    'block-no-empty': true,
+    // Force all colors to be declared as `oklch()`
+    'color-named': 'never',
+    'function-disallowed-list': ['rgb', 'rgba', 'hsl', 'hsla', 'hex', 'hwb', 'lab', 'lch', 'oklab']
+  }
 };
